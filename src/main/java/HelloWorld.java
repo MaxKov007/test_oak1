7c3a5fb0cf20e9e37839fe009548c61f848a399d
Volodymyr Verbovyi <v.me@me.com>
2020-04-20 13:44:03 +0300

    Cement

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int myName;
-        myName = 666;
+        int myName;//Переменная
+        myName = 666; //Имя Переменная
 
         System.out.println("Hello World" + myName + "Почему нет пробела?");
         System.out.println("Где пробел?");
