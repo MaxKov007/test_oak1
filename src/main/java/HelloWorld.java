3b652c0b14e5c4f722c10ef0a02a9c4f0f946002
Volodymyr-verbovyi <v.me@me.com>
2020-04-20 13:38:22 +0300

    Project Who Space?

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println(1);
+        int myName;
+        myName = 666;
+
+        System.out.println("Hello World" + myName + "Почему нет пробела?");
+        System.out.println("Где пробел?");
     }
 }
