771e0f8294b5b4f04c002be58320cdebe05f0af0
Ruslan <ruslan.burd@gmail.com>
2020-04-22 13:15:26 +0300

    changed string, added char

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int myName;//Переменная
-        myName = 666; //Имя Переменная
+        int myAge;//Переменная
+        myAge = 666; //Имя Переменная
+        String myName2 = "Andrey";
+        char a = 'a';
 
-        System.out.println("Hello World" + myName + "Почему нет пробела?");
+        System.out.println("Hello World " + myAge + myName2 + " Почему нет пробела?" +" ");
         System.out.println("Где пробел?");
 
         System.out.println("Master");
