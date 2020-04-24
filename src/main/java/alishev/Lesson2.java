a1e041d941cdbb070f620cc36f82fa14dad25a2d
KovalchukMax <kovalchukmax@gmail.com>
2020-04-24 20:18:34 +0300

    Alishev lessons

---
+++ src/main/java/alishev/Lesson2.java
@@ -0,0 +1,29 @@
+package alishev;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        int myInt = 557;
+        short myShort = 3266;
+        long myLong = 354654654;
+
+        double myDouble = 235.35;
+        float myFloat = 2362.4f;
+
+        char c = 'a';
+        boolean b = true;
+
+        byte myByte = 100; //-128-127
+
+        System.out.println(myInt);
+        System.out.println(myShort);
+        System.out.println(myLong);
+
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+
+        System.out.println(c);
+        System.out.println(b);
+
+        System.out.println(myByte);
+    }
+}
---
+++ src/main/java/alishev/Lesson3.java
@@ -0,0 +1,15 @@
+package alishev;
+
+public class Lesson3 {
+    public static void main(String[] args) {
+        int x = 5;
+        String s = "Hello"; // класс - пишеться с большой буквы
+        String space = " ";
+        String name = "Bob";
+        System.out.println(s + space + name);
+        System.out.println("Hello" + " " + "John");
+        System.out.println("My number is " + x);
+
+
+    }
+}
